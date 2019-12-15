2e6f2f25bd5493b0b7f58cb84284e830cc7c6b21
Michael Miles <michael@miles-me.net>
2019-12-15 07:07:54 -0600

    Update TupleChallenge.py
    Created my solution along with a combined solution, his and mine.

--- Section5-ListsRangesTuples/TupleChallenge.py
+++ Section5-ListsRangesTuples/TupleChallenge.py
@@ -10,11 +10,15 @@ imelda = ("More Mayhem", "Imilda May", 2011, ((1, "Pulling the Rug"), (2, "Psych
           (4, "Kentish Town Waltz")))
 
 # My Solution
+print("My Solution")
 print("Title: {}\nArtist: {}\nReleased: {}".format(imelda[0], imelda[1], imelda[2]))
-for x in range(3, 7):
-    print("\ttrack: {}, {}".format(imelda[x][0], imelda[x][1]))
+for track in range(4):
+    print("\tTrack: {}, {}".format(imelda[3][track][0], imelda[3][track][1]))
+print("*" * 40 + "\n")
+
 
 # His Solution
+print("His Solution")
 title, artist, year, tracks = imelda
 print(title)
 print(artist)
@@ -22,5 +26,12 @@ print(year)
 for song in tracks:
     track, title = song
     print("\tTracks number {}, Title: {}".format(track, title))
+print("*" * 40 + "\n")
 
 # Combination Solution
+print("Combined Solution")
+title, artist, year, tracks = imelda
+print("Title: {}\nArtist: {}\nReleased: {}".format(title, artist, year))
+for song in tracks:
+    track, title = song
+    print("\tTrack: {}, {}".format(track, title))

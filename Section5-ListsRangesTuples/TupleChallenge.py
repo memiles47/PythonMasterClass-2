871d8c6ae7b677f160f99a20bf4c7945204f05c8
Michael Miles <michael@miles-me.net>
2019-12-15 05:58:50 -0600

    Update TupleChallenge.py
    Adding to course and taking challenge.

--- Section5-ListsRangesTuples/TupleChallenge.py
+++ Section5-ListsRangesTuples/TupleChallenge.py
@@ -6,6 +6,21 @@ __author__ = 'Michael E Miles'
 # Indent the tracks by a single tab stop when printing them (remember that you can pass
 # more than one item to the print function, separating them with a comma).
 
-imelda = ("More Mayhem", "Imilda May", 2011, (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"),
-          (4, "Kentish Town Waltz"))
+imelda = ("More Mayhem", "Imilda May", 2011, ((1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"),
+          (4, "Kentish Town Waltz")))
 
+# My Solution
+print("Title: {}\nArtist: {}\nReleased: {}".format(imelda[0], imelda[1], imelda[2]))
+for x in range(3, 7):
+    print("\ttrack: {}, {}".format(imelda[x][0], imelda[x][1]))
+
+# His Solution
+title, artist, year, tracks = imelda
+print(title)
+print(artist)
+print(year)
+for song in tracks:
+    track, title = song
+    print("\tTracks number {}, Title: {}".format(track, title))
+
+# Combination Solution

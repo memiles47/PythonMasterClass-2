87a1375c6e862d97e6d51bbec1a020559b3b59e2
Michael Miles <michael@miles-me.net>
2019-12-21 10:30:19 -0600

    Update TupleChallenge.py
    Created a list of tuples and the results turned out the same.

--- Section5-ListsRangesTuples/TupleChallenge.py
+++ Section5-ListsRangesTuples/TupleChallenge.py
@@ -6,8 +6,8 @@ __author__ = 'Michael E Miles'
 # Indent the tracks by a single tab stop when printing them (remember that you can pass
 # more than one item to the print function, separating them with a comma).
 
-imelda = ("More Mayhem", "Imilda May", 2011, ((1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"),
-          (4, "Kentish Town Waltz")))
+imelda = ("More Mayhem", "Imilda May", 2011, [(1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"),
+          (4, "Kentish Town Waltz")])
 
 # My Solution
 print("My Solution")

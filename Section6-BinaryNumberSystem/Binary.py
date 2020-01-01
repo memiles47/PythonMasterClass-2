c61e0d25eb5598c84bcbcd9aab4b41345d4701d7
Michael Miles <michael@miles-me.net>
2020-01-01 17:19:42 -0600

    Update Binary.py
    Added hexidecimal equiv

--- Section6-BinaryNumberSystem/Binary.py
+++ Section6-BinaryNumberSystem/Binary.py
@@ -2,4 +2,4 @@ __author__ = 'Michael E Miles'
 
 # This is also another demonstration of formatting
 for i in range(17):
-    print("{0:>2} in binary is {0:>08b}".format(i))
+    print("{0:>2} in binary is {0:>08b} in hex is {0:>02x}".format(i))

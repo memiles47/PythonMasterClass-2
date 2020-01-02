bb761480cad931c9a8115ac97cfe94c7ad205eed
Michael Miles <michael@miles-me.net>
2020-01-01 18:10:48 -0600

    Update Binary.py

--- Section6-BinaryNumberSystem/Binary.py
+++ Section6-BinaryNumberSystem/Binary.py
@@ -1,5 +1,14 @@
 __author__ = 'Michael E Miles'
 
 # This is also another demonstration of formatting
-for i in range(17):
-    print("{0:>2} in binary is {0:>08b} in hex is {0:>02x}".format(i))
+for i in range(257):
+    print("{0:>3} in binary is {0:>09b} in hex is {0:>03x} in Octal is {0:>03o}".format(i))
+
+x = 0x20
+y = 0x0a
+
+print(x)
+print(y)
+print(x * y)
+
+print(0b101010)

50b80be4928224321a708c5995825cdce0310bc2
Michael Miles <michael@miles-me.net>
2020-01-05 07:47:39 -0600

    Working Section 7

--- Section7-PythonDictionariesAndSets/Dictionaries.py
+++ Section7-PythonDictionariesAndSets/Dictionaries.py
@@ -1,2 +1,27 @@
 __author__ = "Michael E Miles"
 
+fruit = {"orange": "A sweet, orange, citrus fruit",
+         "apple": "Good for making cider",
+         "lemon": "A sour, yello citrus fruit",
+         "grape": "A small, sweet fruit growing in bunches",
+         "lime": "A sour, green citrus fruit"}
+# print(fruit)
+# print(fruit["lemon"])
+# fruit["pear"] = "An odd shaped apple"
+# print(fruit)
+# fruit["lime"] = "Great with tequila"
+# print(fruit)
+# # del fruit["lemon"]
+# # fruit.clear()
+# # print(fruit)
+# print(fruit["tomato"]
+print(fruit)
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    if dict_key in fruit:
+        description = fruit.get(dict_key)
+        print(description)
+    else:
+        print("We don't have a " + dict_key)

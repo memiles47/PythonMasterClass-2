fe56c994caf53986e351fedb8e339b306ea1bff9
Michael Miles <michael@miles-me.net>
2020-01-06 03:29:16 -0600

    Update Dictionaries.py

--- Section7-PythonDictionariesAndSets/Dictionaries.py
+++ Section7-PythonDictionariesAndSets/Dictionaries.py
@@ -15,13 +15,31 @@ fruit = {"orange": "A sweet, orange, citrus fruit",
 # # fruit.clear()
 # # print(fruit)
 # print(fruit["tomato"]
-print(fruit)
-while True:
-    dict_key = input("Please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("We don't have a " + dict_key)
+# print(fruit)
+# while True:
+#     dict_key = input("Please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#     # description = fruit.get(dict_key, "We don't have a " + dict_key)
+#     # print(description)
+#     if dict_key in fruit:
+#         description = fruit.get(dict_key)
+#         print(description)
+#     else:
+#         print("We don't have a " + dict_key)
+
+
+# orderedKeys = list(fruit.keys())
+# orderedKeys.sort()
+orderedKeys = sorted(list(fruit.keys()))
+for f in orderedKeys:
+    print(f + " - " + fruit[f])
+print("*" * 40)
+for f in sorted(fruit.keys()):
+    print(f + " - " + fruit[f])
+print("*" * 40)
+for val in fruit.values():
+    print(val)
+print("*" * 40)
+
+
